9a488481fe8ed02e3aa81f712207af4906121694
ReemziP <rp141094@gmail.com>
2016-10-09 14:25:45 -0400

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Enter Nick Name&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtNickName" runat="server" Width="168px"></asp:TextBox>
+        <br />
+        <br />
+        Generated Number&nbsp;
+        <asp:TextBox ID="txtGeneratedNo" runat="server" Enabled="False"></asp:TextBox>
+        <br />
+        <br />
+        Guess a Number&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtGuessedNumber" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Guesses Made&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="txtGuessesMade" runat="server" Enabled="False"></asp:TextBox>
+        <br />
+        <br />
+        Guesses Remaining
+        <asp:TextBox ID="txtGuessesRemaining" runat="server" Enabled="False"></asp:TextBox>
+        <br />
+        <br />
+        Score&nbsp;
+        <asp:TextBox ID="txtScore" runat="server" Enabled="False"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="lblOutput" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Button ID="btnGuessNumber" runat="server" OnClick="Button1_Click" Text="Guess Number" />
+&nbsp;&nbsp;
+        <asp:Button ID="btnViewScores" runat="server" OnClick="btnViewScores_Click" Text="View Scores" Width="102px" />
+&nbsp;&nbsp;
+        <asp:Button ID="btnRestartGame" runat="server" OnClick="Button2_Click" Text="Restart Game" />
+    
+        <br />
+    
+        <br />
+        <asp:Table ID="tblViewScore" runat="server" Visible="False">
+            <asp:TableHeaderRow>
+                <asp:TableHeaderCell>Nick Name</asp:TableHeaderCell>
+                <asp:TableHeaderCell>Score</asp:TableHeaderCell>
+            </asp:TableHeaderRow>
+        </asp:Table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+using System.Text;
+using System.IO;
+
+public partial class _Default : System.Web.UI.Page
+{
+    private static byte clickCount = 0;
+    private static byte randomNumber = 0;
+    private string filePath = @"D:\scores.txt";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        clickCount++;       
+
+        Debug.WriteLine("clickCount: " + clickCount);
+
+        if (clickCount == 1)
+        {
+            Random random = new Random();
+            randomNumber = Convert.ToByte(random.Next(1, 100));
+            Debug.WriteLine("randomNumber: " + randomNumber);
+        }
+
+        if (clickCount == 100)
+        {
+            btnGuessNumber.Enabled = false;
+        }
+
+        byte userInput = Convert.ToByte(txtGuessedNumber.Text);
+        byte clickRemaining = Convert.ToByte(100 - clickCount);
+
+        txtGuessesMade.Text = Convert.ToString(clickCount);
+        txtGuessesRemaining.Text = Convert.ToString(clickRemaining);
+
+        if (randomNumber > userInput)
+        {
+            lblOutput.Text = "Too low, try again.";
+        }
+        else if (randomNumber < userInput)
+        {
+            lblOutput.Text = "Too high, try again.";
+        }
+        else
+        {
+            lblOutput.Text = "Congratulations. You win!";
+            txtGeneratedNo.Text = Convert.ToString(randomNumber);
+            txtScore.Text = Convert.ToString(clickRemaining);
+
+            storeHighScore(txtNickName.Text, clickRemaining);
+        }      
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        clickCount = 0;
+        randomNumber = 0;
+
+        txtNickName.Text = "";
+        txtScore.Text = "";
+        txtGuessesRemaining.Text = "";
+        txtGuessesMade.Text = "";
+        txtGuessedNumber.Text = "";
+        txtGeneratedNo.Text = "";
+
+        lblOutput.Text = "";
+
+        tblViewScore.Visible = false;
+
+        btnGuessNumber.Enabled = true;
+    }
+
+    protected void btnViewScores_Click(object sender, EventArgs e)
+    {
+        viewScores();
+    }
+
+    private void storeHighScore(string nickName, byte score)
+    {
+        string [] setLines = new string[1];
+
+        setLines[0] = nickName+"|"+score;
+
+        File.AppendAllLines(filePath, setLines, Encoding.UTF8);        
+    }
+
+    private void viewScores()
+    {
+        tblViewScore.Visible = true;
+
+        string[] getLines = File.ReadAllLines(filePath);
+        string [] split;
+
+        foreach (string line in getLines)
+        {
+            split = line.Split('|');
+
+            TableRow row = new TableRow();
+            TableCell cell1 = new TableCell();
+            cell1.Text = split[0]; //Nick name
+
+            TableCell cell2 = new TableCell();
+            cell2.Text = split[1]; //scores
+
+            row.Cells.Add(cell1);
+            row.Cells.Add(cell2);
+
+            tblViewScore.Rows.Add(row);
+        }
+    }
+}
